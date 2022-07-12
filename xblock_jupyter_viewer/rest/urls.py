6f7f305233d12db02f5dca206d930bb4cc493011
crivet <c.rivet@louisiana.edu>
2022-07-12 09:00:29 -0500

    upated rest/urls.py to use django.urls.path instead of django.conf.urls.url

--- xblock_jupyter_viewer/rest/urls.py
+++ xblock_jupyter_viewer/rest/urls.py
@@ -1,15 +1,15 @@
 """
 Defines a URL to return a notebook html page to be used in an iframe
 """
-from django.conf.urls import url
-
+# from django.conf.urls import url
+from django.urls import path
 from .views import NotebookViewer
 
 app_name = 'xblock_jupyter_viewer'
 
 urlpatterns = [
-    url(
-    r'^render_notebook/$',
+    path(
+    'render_notebook/',
         NotebookViewer.as_view(),
         name='jupyter_nb_viewer'
     )
